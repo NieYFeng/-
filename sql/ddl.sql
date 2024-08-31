d1a35a33bfa4b40962fca5a2d906580ab44db221
nick <1097583046@qq.com>
2024-08-31 23:20:02 +0800

    项目初始化及菜品和用户的数据库表设计

--- sql/ddl.sql
+++ sql/ddl.sql
@@ -9,16 +9,13 @@ create table if not exists user
 (
     id           bigint auto_increment comment 'id' primary key,
     userName     varchar(256)                           null comment '用户昵称',
-    userAccount  varchar(256)                           not null comment '账号',
+    openid       VARCHAR(128) NOT NULL UNIQUE,          -- 微信用户的唯一标识
     userAvatar   varchar(1024)                          null comment '用户头像',
     gender       tinyint                                null comment '性别',
     userRole     varchar(256) default 'user'            not null comment '用户角色：user / admin',
-    userPassword varchar(512)                           not null comment '密码',
     createTime   datetime     default CURRENT_TIMESTAMP not null comment '创建时间',
     updateTime   datetime     default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete     tinyint      default 0                 not null comment '是否删除',
-    constraint uni_userAccount
-        unique (userAccount)
 ) comment '用户';
 
 -- 帖子表
@@ -43,3 +40,17 @@ create table if not exists post
     updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
     isDelete      tinyint  default 0                 not null comment '是否删除'
 ) comment '帖子';
+
+create table if not exists dish
+(
+    id           bigint auto_increment comment 'id' primary key,
+    homeId       bigint,                              -- 菜品表中的家的ID字段
+    dishName     varchar(256)                       null comment '菜品名称'
+    dishText     varchar(256)                       null comment '菜品描述'
+    dishPhoto    varchar(256)                       null comment '菜品图片'
+    price        tinyint default 0                  not null default 0 comment '菜品价格'
+    catagory     varchar(256)                       null comment '菜品类别'
+    updateTime    datetime default CURRENT_TIMESTAMP not null on update CURRENT_TIMESTAMP comment '更新时间',
+    createTime    datetime default CURRENT_TIMESTAMP not null comment '创建时间',
+    isDelete      tinyint  default 0                 not null comment '是否删除'
+)
